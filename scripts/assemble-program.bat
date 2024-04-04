cf5346a77d53fbfde7ce068ee001eb3a900f5844
Bella <2770706h@student.gla.ac.uk>
2024-04-04 16:33:38 +0100

    automated assembly

--- scripts/assemble-program.bat
+++ scripts/assemble-program.bat
@@ -1,1 +1,2 @@
+cd..
 .\Uxn32.exe .\drifblim.rom .\dynamic-memory-alloc-skeleton.tal .\dynamic-memory-alloc-skeleton.rom
--- scripts/assemble-tests.bat
+++ scripts/assemble-tests.bat
@@ -1,1 +1,2 @@
+cd..
 .\Uxn32.exe .\drifblim.rom .\dynamic-memory-alloc-unit-tests.tal .\dynamic-memory-alloc-unit-tests.rom
