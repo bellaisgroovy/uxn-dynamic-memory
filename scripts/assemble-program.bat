5c214e2c8fec4c71ef09e58c2aff9554366f791d
Bella <2770706h@student.gla.ac.uk>
2024-04-04 12:45:50 +0100

    organised into folders

---
+++ scripts/assemble-program.bat
@@ -0,0 +1,1 @@
+.\Uxn32.exe .\drifblim.rom .\dynamic-memory-alloc-skeleton.tal .\dynamic-memory-alloc-skeleton.rom
---
+++ scripts/assemble-tests.bat
@@ -0,0 +1,1 @@
+.\Uxn32.exe .\drifblim.rom .\dynamic-memory-alloc-unit-tests.tal .\dynamic-memory-alloc-unit-tests.rom
